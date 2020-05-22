cefaae068657c885fb3b4500f061d62e470feb73
SnyderMbishai <csmbishai@gmail.com>
2020-05-22 14:37:04 +0300

    Add styling to the templates

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
-
-admin.site.register(Question)
+from .models import Question, Choice
 
 # Register your models here.
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]    
+    # fields = ['pub_date', 'question']
+    list_display = ('question', 'pub_date', 'was_recently_published')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+admin.site.register(Question, QuestionAdmin)
